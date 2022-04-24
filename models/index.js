2c533ce887eac82814e4191674177a5a086fa012
Cole <deoncole77@gmail.com>
2022-04-24 11:55:59 -0400

    Post model set up

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 // require the data from the User model
 const User = require('./User');
+// require the data from the Post model
+const Post = require('./Post');
 
-// export the User object
-module.exports = { User };
+// export the User & Post object
+module.exports = { User, Post };
