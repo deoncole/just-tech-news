55f804c3da2a5b9472d092174c178f301603ec5a
Cole <deoncole77@gmail.com>
2022-04-25 12:22:11 -0400

    complete CRUD for post

--- models/index.js
+++ models/index.js
@@ -3,5 +3,15 @@ const User = require('./User');
 // require the data from the Post model
 const Post = require('./Post');
 
+// create associations to how the tables will relate to each other setting the user id as the foreign key
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+// make the reverse association of the post table to the user table. this says that the post can belong to one user but not many
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});  
+
 // export the User & Post object
 module.exports = { User, Post };
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,9 @@
 const router = require('express').Router();
 //require the userroutes file
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
 // use the routes appending the /users prefix to the routes
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
